6c6a526ad8b16ae077e66cf47a46b8abd3815034
Asep Koha <asepkoharudin04@gmail.com>
2026-05-07 14:12:10 +0700

    chore: setup env config and gitignore

---
+++ src/config/env.ts
@@ -0,0 +1,5 @@
+export const ENV = {
+  appName: import.meta.env.VITE_APP_NAME ?? 'Lambung Tenang',
+  appVersion: import.meta.env.VITE_APP_VERSION ?? '1.0.0',
+  apiUrl: import.meta.env.VITE_API_URL ?? '',
+};
